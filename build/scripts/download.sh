530f47cef0107b509a2e0c8e49679422e7838696
Vitor Eiken <117394582+eikqn@users.noreply.github.com>
2026-05-19 16:32:02 -0300

    feat(script): choose a path to download
    also make sure that wgeted files have the right name (using -O flag with wget)

--- build/scripts/download.sh
+++ build/scripts/download.sh
@@ -4,8 +4,12 @@ KERNEL_FAMILY="v6.x"
 KERNEL_VERSION="linux-6.19.13"
 BUSYBOX_VERSION="busybox-1.36.1"
 
-wget https://cdn.kernel.org/pub/linux/kernel/${KERNEL_FAMILY}/${KERNEL_VERSION}.tar.xz
-tar -xf ${KERNEL_VERSION}.tar.xz
-
-wget https://busybox.net/downloads/${BUSYBOX_VERSION}.tar.bz2
-tar -xf ${BUSYBOX_VERSION}.tar.bz2
+if [ $# -eq 1 ]; then
+  cd $1 &&
+  wget -O ${KERNEL_VERSION}.tar.xz https://cdn.kernel.org/pub/linux/kernel/${KERNEL_FAMILY}/${KERNEL_VERSION}.tar.xz &&
+  tar -xf ${KERNEL_VERSION}.tar.xz &&
+  wget -O ${BUSYBOX_VERSION}.tar.bz2 https://busybox.net/downloads/${BUSYBOX_VERSION}.tar.bz2 &&
+  tar -xf ${BUSYBOX_VERSION}.tar.bz2
+else
+  echo "Usage: $0 <download_path>"
+fi
